1f99e7a741e1a3238c85a58a62ca42d58c9ccaa0
Mândrescu Ștefan <70579287+RC014@users.noreply.github.com>
2022-09-17 16:47:36 +0300

    Update tda.py

--- tda.py
+++ tda.py
@@ -1,88 +1,68 @@
 a,b = int(input("nr1= ")),int(input("nr2= "))
 
-def suma(a,b):
-    return a+b
-
+def suma(a,b):return a+b
 print("Suma= ",suma(a,b))
 
-def produs(a,b):
-    return a*b
-
+def produs(a,b):return a*b
 print("Produsul= ",produs(a,b))
 
-def media(a,b):
-    return (a+b)/2
-
+def media(a,b):return (a+b)/2
 print("Media aritmetica= ",media(a,b))
 
-c=[]
+divizori=[]
 def max_div(a, b):
     n=0
     for i in range(1, min(a, b)+1):
         if a%i==b%i==0:
             n+=1
-            c.append(i)
+            divizori.append(i)
 max_div(a, b)
 
-print("Cel mai mare divizor comun= ",c[-1])
+print("Cel mai mare divizor comun= ",divizori[-1])
 
 d=[]
-def min_mult(a, b):
-    return (a * b) // c[-1]
+def min_mult(a, b):return (a * b) // divizori[-1]
 
 print("Cel mai mic multiplu comun= ",min_mult(a, b))
-
 print("Numarul minim= ",min(a,b))
-
 print("Numarul maxim= ",max(a,b))
 
-def suma_f():
-    a,b = int(input("nr1= ")),int(input("nr2= "))
-    print(a+b)
-
+def suma_f():a,b = int(input("nr1= ")),int(input("nr2= "));print("Suma= ",a+b)
 suma_f()
 
-def produs_f():
-    a,b = int(input("nr1= ")),int(input("nr2= "))
-    print(a*b)
-
+def produs_f():a,b = int(input("nr1= ")),int(input("nr2= "));print("Produsul= ",a*b)
 produs_f()
 
-print("Toti divizorii comuni: ",end="")
-print(*c,sep=' ; ')
+print("Toti divizorii comuni:",*divizori,sep=' ')
 
-m=[]
+multipli=[]
 def mult_com_5(a,b):
     x=a*b
     for i in range(5):
-        m.append(x)
+        multipli.append(x)
         x=x*2
 mult_com_5(a,b)
 
-print("Cinci multipli comuni: ",end="")
-print(*m,sep=' ; ')
+print("Cinci multipli comuni:",*multipli,sep=' ')
 
-list_a,list_b=[],[]
+digits_a,digits_b=[],[]
 def comun(a,b):
-    list_a.append([int(x) for x in str(a)])
-    list_b.append([int(x) for x in str(b)])
+    digits_a.append([int(x) for x in str(a)])
+    digits_b.append([int(x) for x in str(b)])
     c=[*set([x for x in str(a) if x in str(b)])]
     p=[*set([x for x in str(a) if x not in str(b)])]
     c.sort();p.sort()
     return c,p
 
-print("Cifrele comune: ",end="")
-print(*(comun(a,b)[0]),sep=' ; ')
+print("Cifrele comune:",*(comun(a,b)[0]),sep=' ')
 
-print("Cifrele care se regasesc doar in primul numar: ",end="")
-print(*(comun(a,b)[1]),sep=' ; ')
+print("Cifrele care se regasesc doar in primul numar: ",*(comun(a,b)[1]),sep=' ')
 
 def nr_div(n):
-    c=[]
+    div=[]
     for i in range(1,n+1):
         if n%i==0:
-            c.append(i)
-    return(len(c))
+            div.append(i)
+    return(len(div))
 
-if nr_div(a)==nr_div(b):
-    print("PRIETENE")
+if nr_div(a)==nr_div(b):print("PRIETENE")
